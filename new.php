bcde96e0389069a1fade2c734f7a8148b46641f2
itsujay <sujaymajisukumar7439@gmail.com>
2024-09-19 11:05:43 +0530

    php file added

---
+++ new.php
@@ -0,0 +1,10 @@
+<?php
+$s="My Name is ItSujay";
+echo str_word_count($s);
+echo"<br>";
+echo strpos($s,"is")."<br>";
+echo strtoupper($s)."<br>";
+$c="Good Morning"."<br>";
+echo str_replace('Morning','Night',$c)."<br>";
+echo ($c);
+?>
